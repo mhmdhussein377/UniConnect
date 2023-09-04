30df5042fad7a20514704fe56ff0ca0f4406151b
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-04 11:49:23 +0300

    created a route to register

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const {mongoConnection} = require("./utils/mongoConnection")
+const verifyToken = require("./utils/verifyToken")
+const { handleError } = require("./utils/error")
 
 const app = express()
 
@@ -12,5 +14,12 @@ app.use(cors())
 app.listen(process.env.PORT, () => {
     console.log("Server connected");
 });
+
+// Routes
+const AuthRoutes = require("./routes/AuthRoutes")
+
+app.use(verifyToken)
+
+app.use(handleError)
 
 mongoConnection
--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -2,4 +2,33 @@ const User = require("./../models/User")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const dotenv = require("dotenv")
+const {createError} = require("./../utils/error")
 dotenv.config()
+
+const Register = async(req, res, next) => {
+    const {name, username, email, password} = req.body;
+
+    try {
+        const usernameExists = await User.findOne({username});
+        if (usernameExists) 
+            return next(createError(400, "Invalid username!"));
+        
+        const existingUser = await User.findOne({email});
+        if (existingUser) 
+            return next(createError(400, "User already exists! Login Instead"));
+        
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = new User({name, username, email, password: hashedPassword});
+        await user.save();
+
+        res
+            .status(201)
+            .json({message: "user created successfully"});
+    } catch (error) {
+        res
+            .status(500)
+            .json(error);
+    }
+};
