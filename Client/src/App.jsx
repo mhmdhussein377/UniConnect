966994abc776fe7f12c544de35136ae0aeb86190
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-06 12:29:59 +0300

    created a section for the user data in the profile page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Auth/Login"
 import Register from "./pages/Auth/Register"
 import Home from "./pages/Home"
 import CommunityProfile from "./pages/CommunityProfile"
+import Profile from "./pages/Profile"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
+      <Route path='/profile' element={<Profile/>} />
       <Route path="/community/:id" element={<CommunityProfile />} />
     </Routes>
   );
