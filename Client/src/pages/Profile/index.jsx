98ec187a8f3d86ad50f86ae9ec504eb3282d4cd8
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-06 13:47:16 +0300

    created a component for the educational background in the profile page

--- Client/src/pages/Profile/index.jsx
+++ Client/src/pages/Profile/index.jsx
@@ -55,7 +55,26 @@ const index = () => {
                             <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolore corrupti laudantium cupiditate, iure eum explicabo voluptatum impedit non, obcaecati ipsa distinctio minima aspernatur alias modi! Iste voluptatem provident inventore!</p>
                         </div>
                     </div>
-                    <div className="flex-[3]"></div>
+                    <div className="flex-[3] bg-white drop-shadow-lg max-w-full p-4 rounded-md h-fit flex flex-col gap-4">
+                        <div className="flex flex-col gap-3">
+                            <div className="flex items-center justify-between font-semibold text-lg">
+                                Educational Information
+                                <HiPencil className="cursor-pointer" size={30} />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <div className="flex">
+                                    <div className="w-[100px] font-medium">
+                                        University
+                                    </div>
+                                    <span className="text-gray-500 max-w-[270px] overflow-hidden text-ellipsis whitespace-nowrap">University of Toronto</span>
+                                </div>
+                                <div className="flex">
+                                    <div className="w-[100px] font-medium">Major</div>
+                                    <span className="text-gray-500">Computer Science</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
