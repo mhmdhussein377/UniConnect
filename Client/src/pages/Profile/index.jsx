9e0c9d4461381b71f16a153fdcd9705718015a9d
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-06 13:39:34 +0300

    created a section for the user bio in the profile page

--- Client/src/pages/Profile/index.jsx
+++ Client/src/pages/Profile/index.jsx
@@ -49,6 +49,11 @@ const index = () => {
                                 </div>
                             </div>
                         </div>
+                        {/* BIO */}
+                        <div className="bg-white drop-shadow-lg rounded-md p-4 flex flex-col gap-3">
+                            <div className="text-xl font-semibold">About</div>
+                            <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolore corrupti laudantium cupiditate, iure eum explicabo voluptatum impedit non, obcaecati ipsa distinctio minima aspernatur alias modi! Iste voluptatem provident inventore!</p>
+                        </div>
                     </div>
                     <div className="flex-[3]"></div>
                 </div>
