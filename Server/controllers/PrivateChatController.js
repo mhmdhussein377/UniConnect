b8a6b7fafb2323b83d3760a0eebbbed40847b386
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-08 19:53:50 +0300

    created a route to create private messages

--- Server/controllers/PrivateChatController.js
+++ Server/controllers/PrivateChatController.js
@@ -8,12 +8,45 @@ const CreatePrivateConversation = async(req, res) => {
             Members: [userOne, userTwo]
         })
         const savedPrivateConversation = await newPrivateConversation.save()
-        res.status(201).json(savedPrivateConversation)
+        res
+            .status(201)
+            .json(savedPrivateConversation)
     } catch (error) {
-        return res.status(500).json({ error: "Internal server error" });
+        return res
+            .status(500)
+            .json({error: "Internal server error"});
+    }
+}
+
+const CreatePrivateMessage = async(req, res) => {
+    const {sender, receiver, content} = req.body
+
+    try {
+        await PrivateConversation.findOneAndUpdate({
+            Members: {
+                $in: [sender, receiver]
+            }
+        }, {
+            $push: {
+                messages: {
+                    sender,
+                    content
+                }
+            }
+        });
+
+        if(!PrivateConversation)
+            return res.status(404).json({error: "Private conversation not found"})
+
+        res.status(200).json("Message saved successfully")
+    } catch (error) {
+        return res
+            .status(500)
+            .json({error: "Internal server error"});
     }
 }
 
 module.exports = {
-    CreatePrivateConversation
+    CreatePrivateConversation,
+    CreatePrivateMessage
 }
--- Server/routes/PrivateChat.js
+++ Server/routes/PrivateChat.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {CreatePrivateConversation} = require("../controllers/PrivateChatController")
+const {CreatePrivateConversation, CreatePrivateMessage} = require("../controllers/PrivateChatController")
 
 // create a new private conversation
 router.post('/newPrivateConversation', CreatePrivateConversation)
+
+// create a new private message
+router.post("/newPrivateMessage", CreatePrivateMessage)
 
 module.exports = router;
