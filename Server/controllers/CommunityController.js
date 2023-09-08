078b7bf54bc50c69ed7db5667fd82e3ec2051351
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-08 19:13:07 +0300

    created a route to create communities

--- Server/controllers/CommunityController.js
+++ Server/controllers/CommunityController.js
@@ -1,4 +1,5 @@
 const Community = require("./../models/Community")
+const User = require("./../models/User")
 
 const getCommunity = async(req, res) => {
     const {communityId} = req.params
@@ -6,13 +7,51 @@ const getCommunity = async(req, res) => {
     try {
         const community = await Community.findById(communityId)
 
-        if(!community)
+        if (!community) 
             return res.status(404).json({error: "Community not found"})
 
-        return res.status(200).json({community})
+        return res
+            .status(200)
+            .json({community})
     } catch (error) {
-        res.status(500).json({ error: "Internal server error" });
+        res
+            .status(500)
+            .json({error: "Internal server error"});
     }
 }
 
-module.exports = {getCommunity}
+const CreateCommunity = async(req, res) => {
+    const {name, description, privacy} = req.body
+    const creator = req.user.id
+
+    try {
+        // check if the creator exists
+        const user = await User.findById(creator)
+        if(!user)
+            return res.status(404).json({error: "Creator not found"})
+
+        const community = new Community({name, description, privacy, creator})
+        const savedCommunity = await community.save()
+
+        user.createdCommunities.push(savedCommunity._id)
+        await user.save()
+
+        return res.status(201).json(savedCommunity)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+const DeleteCommunity = async(req, res) => {}
+
+const UpdateCommunity = async(req, res) => {}
+
+const AddMembers = async(req, res) => {}
+
+module.exports = {
+    getCommunity,
+    CreateCommunity,
+    DeleteCommunity,
+    UpdateCommunity,
+    AddMembers
+};
--- Server/routes/CommunityRoutes.js
+++ Server/routes/CommunityRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {GetCommunity} = require("./../controllers/CommunityController")
+const {GetCommunity, CreateCommunity, DeleteCommunity, UpdateCommunity, AddMembers} = require("./../controllers/CommunityController");
 
 router.get("/:communityId", GetCommunity)
+
+router.post("/create", CreateCommunity);
+
+router.post("/delete/:communityId", DeleteCommunity);
+
+router.post("update/:communityId", UpdateCommunity);
+
+router.post("/:communityId/add-members", AddMembers);
 
 module.exports = router;
