7520402b8044dc2f78b39fdf90d2d00812f92c17
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-16 01:49:57 +0300

    implemented the functionality to change the profile and cover images in the frontend side

--- Client/src/components/UserDataSection/index.jsx
+++ Client/src/components/UserDataSection/index.jsx
@@ -15,8 +15,10 @@ const index = ({setShowEditUserModal, user, isCurrentUser}) => {
         setButtonText] = useState("")
     const [loading,
         setLoading] = useState(false);
-    const coverPicRef = useRef();
-    const profilePicRef = useRef();
+    const coverPicInputRef = useRef();
+    const profilePicInputRef = useRef();
+    const coverImg = useRef()
+    const profileImg = useRef()
 
     useEffect(() => {
         const getFrienship = async() => {
@@ -96,7 +98,7 @@ const index = ({setShowEditUserModal, user, isCurrentUser}) => {
         }
     }
 
-    const handleInput = (e) => {
+    const handleInput = (e, image) => {
         if (e.target.files.length > 0) {
             function getBase64(file) {
                 return new Promise((resolve, reject) => {
@@ -108,7 +110,7 @@ const index = ({setShowEditUserModal, user, isCurrentUser}) => {
             }
             // setInput(e.target.files[0]);
             getBase64(e.target.files[0]).then((data) => {
-                imageRef.current.src = data;
+                image === "cover" ? coverImg.current.src = data : profileImg.current.src = data
             });
 
             const reader = new FileReader();
@@ -123,19 +125,22 @@ const index = ({setShowEditUserModal, user, isCurrentUser}) => {
         <div className="bg-white drop-shadow-lg rounded-md p-4 flex flex-col gap-3">
             <div className="relative">
                 <img
+                    ref={coverImg}
                     className="h-[200px] w-full object-cover rounded-md"
                     src="https://img.freepik.com/free-photo/landscape-lake-surrounded-by-mountains_23-2148215162.jpg?w=1060&t=st=1693667013~exp=1693667613~hmac=cbe76fdbc4c315a22be9518049b4ce73ba01a29d7839bc73212e6627c7fe2bd3"
                     alt="cover-picture"/>
                 <img
-                    onClick={() => isCurrentUser && profilePicRef.current.click()}
+                    ref={profileImg}
+                    onClick={() => isCurrentUser && profilePicInputRef.current.click()}
                     className={`absolute w-[160px] h-[160px] rounded-full object-cover -bottom-[25%] left-[5%] border-[5px] border-white ${isCurrentUser && "cursor-pointer"}`}
                     src="https://img.freepik.com/free-photo/profile-shot-aristocratic-girl-blouse-with-frill-lady-with-flowers-her-hair-posing-proudly-against-blue-wall_197531-14304.jpg?w=360&t=st=1693254715~exp=1693255315~hmac=11fc761d3797e16d0e4b26b5b027e97687491af623985635a159833dfb9f7826"
                     alt="profile-picture"/>
-                <input ref={profilePicRef} type="file" className="hidden"/> {isCurrentUser && <div
-                    onClick={() => coverPicRef.current.click()}
+                <input onChange={e => handleInput(e, "profile")} ref={profilePicInputRef} type="file" className="hidden"/>
+                {isCurrentUser && <div
+                    onClick={() => coverPicInputRef.current.click()}
                     className="absolute w-[30px] h-[30px] flex items-center justify-center bg-white rounded-full top-2 right-2 cursor-pointer">
                     <HiPencil className="text-primary" size={25}/>
-                    <input ref={coverPicRef} type="file" className="hidden"/>
+                    <input onChange={e => handleInput(e, "cover")} ref={coverPicInputRef} type="file" className="hidden"/>
                 </div>}
             </div>
             <div>
