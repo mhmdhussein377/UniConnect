7ac7e6c6b5d83d84c61e95572e2225280061781c
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-17 15:31:09 +0300

    created a function to get users in a conversation

--- socket/controllers/UserController.js
+++ socket/controllers/UserController.js
@@ -10,3 +10,22 @@ const addUser = async(userId, socketId) => {
         return res.status(500).json({message: "Internal server error"})
     }
 }
+
+// Function to get users in a conversation
+const getUsers = async(sender, receiver) => {
+    try {
+        const conversation = await Conversation.findOne({Members: {
+            $all: [sender, receiver]
+        }}).populate({path: "Members", model: "User"})
+
+        if(!conversation) {
+            res.status(404).json({message: "Conversation not found"})
+        }
+
+        const user = conversation.Members.filter(user => user._id.toString() === receiver)
+        console.log(user[0].socket)
+        return user
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
