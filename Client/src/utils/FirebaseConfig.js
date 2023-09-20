4c9d76dce60544cf3bb0bdca0f325317d3434f9f
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-20 18:52:20 +0300

    initialized firebase in the web app

---
+++ Client/src/utils/FirebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries Your web app's
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDesggNBgm_ElL05FHFIAGHzQGXl5Nrdvg",
+    authDomain: "uniconnect-28324.firebaseapp.com",
+    projectId: "uniconnect-28324",
+    storageBucket: "uniconnect-28324.appspot.com",
+    messagingSenderId: "866764983171",
+    appId: "1:866764983171:web:179a66d8c1b5442f2304b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
