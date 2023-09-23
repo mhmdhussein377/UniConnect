c66b1186716ea641fe12f48d12521729b5572aeb
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-09-23 19:32:11 +0300

    created a route to update the status of notifications

--- Server/controllers/NotificationController.js
+++ Server/controllers/NotificationController.js
@@ -1,20 +1,19 @@
 const Notification = require("./../models/Notification")
 const User = require("./../models/User")
 
-const UpdateNotificationStatus = async(req, res) => {
-    const {notificationId} = req.params
+const UpdateNotificationsStatus = async(req, res) => {
+    const {notifications} = req.body
 
     try {
-        const notification = await Notification.findById(notificationId)
-
-        if (!notification) 
-            return res.status(404).json({error: "Notification not found"})
-
-        await notification.updateOne({isRead: true})
+        await Promise.all(notifications.map(async(notificationId) => {
+            const updatedNotification = await Notification.findByIdAndUpdate(notificationId, {
+                isRead: true
+            }, {new: true})
+        }))
 
         res
             .status(200)
-            .json({message: "Notification marked as read"});
+            .json({message: "Notifications marked as read"});
     } catch (error) {
         res
             .status(500)
@@ -23,41 +22,60 @@ const UpdateNotificationStatus = async(req, res) => {
 }
 
 const GetNotifications = async(req, res) => {
-    const userId = req?.user?.id
+    const userId = req
+        ?.user
+            ?.id
 
     try {
         const user = await User.findById(userId)
-        if(!user) {
-            return res.status(404).json({message: "User not found"})
+        if (!user) {
+            return res
+                .status(404)
+                .json({message: "User not found"})
         }
 
         const notifications = await Notification.find({recipient: userId, isRead: false, status: "pending"})
-        return res.status(200).json({notifications})
+        return res
+            .status(200)
+            .json({notifications})
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        return res
+            .status(500)
+            .json({message: "Internal server error"});
     }
 }
 
 const GetUnreadNotifications = async(req, res) => {
-    const userId = req?.user?.id
+    const userId = req
+        ?.user
+            ?.id
 
     try {
         const user = await User.findById(userId)
 
-        if(!user) {
-            return res.status(404).json({message: "User not found"})
+        if (!user) {
+            return res
+                .status(404)
+                .json({message: "User not found"})
         }
 
-        const unreadNotifications = user.notifications.filter(noti => noti.isRead === false).length
+        const unreadNotifications = user
+            .notifications
+            .filter(noti => noti.isRead === false)
+            .length
 
-        return res.status(400).json({unreadNotifications: unreadNotifications})
+        return res
+            .status(400)
+            .json({unreadNotifications: unreadNotifications})
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        return res
+            .status(500)
+            .json({message: "Internal server error"});
     }
 }
 
 module.exports = {
-    UpdateNotificationStatus,
+    UpdateNotificationsStatus,
     GetNotifications,
     GetUnreadNotifications
 }
--- Server/routes/NotificationRoutes.js
+++ Server/routes/NotificationRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {GetNotifications, GetUnreadNotifications} = require("./../controllers/NotificationController");
+const {GetNotifications, GetUnreadNotifications, UpdateNotificationsStatus} = require("./../controllers/NotificationController");
 
 router.get("/", GetNotifications)
+
+router.post("/updateNotificationsStatus", UpdateNotificationsStatus)
 
 router.get("/unreadNotifications", GetUnreadNotifications)
 
